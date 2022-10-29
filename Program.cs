7eb05a3ea2e888bcbcff1e4861d9c77d5aedc606
Zlataпш <sm-zlata@yandex.ru>
2022-10-29 14:57:45 +0300

    new information

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите трехзначное число ");
+int num=Convert.ToInt32 (Console.ReadLine ());
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount>3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+} 
+else
+{
+    Console.WriteLine(InCenter(num));
+}   
+ int message (string message) 
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
